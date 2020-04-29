824e674cbca06015cbad6abba74753d1ae974c42
yashterror7 <yashbhattm37@gmail.com>
2020-04-29 20:54:47 +0530

    we learned constraint and made chain .js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint= Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintlog,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,7 +15,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    bird = new Bird(100,100);
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -33,8 +34,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    constraintlog = new Log(370,100,120,PI/2);
+    chain = new Chain(bird.body,constraintlog.body);
 
-    bird = new Bird(100,100);
+
+    
 
 }
 
@@ -61,4 +65,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    constraintlog.display();
+    chain.display();
 }
